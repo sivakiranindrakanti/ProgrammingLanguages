1de533852c91ec8e6e57553ecc5b892fdfb87f83
sivakiran <sivakiran0987@gmail.com>
2019-02-02 02:57:43 +0000

    python programming language

---
+++ python/multiplythreenumbers.py
@@ -0,0 +1,5 @@
+Number1 = int(input("To multiply three numbers, enter the first number:"));
+Number2 = int(input("Enter the second number:"));
+Number3 = int(input("Enter the third number:"));
+Product = Number1 * Number2 * Number3
+print("The Product of %d, %d and %d is %d." %(Number1, Number2, Number3, Product))
---
+++ python/sumoftwonumbers.py
@@ -0,0 +1,4 @@
+Number1 = int(input("To find sum of two numbers, enter the first number:"))
+Number2 = int(input("Enter the second number:"))
+print("The sum of %d and %d is %d." %(Number1, Number2, Number1 + Number2))
+
